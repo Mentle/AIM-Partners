a264789b53def9cfbc87ee257a164c91f9c8e421
brightestleaf <marcodietrich23@gmail.com>
2026-01-30 17:39:24 +0100

    Remove CNAME to fix redirect issue

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-cname',
-      closeBundle() {
-        copyFileSync('CNAME', 'docs/CNAME')
-      }
-    }
-  ],
+  plugins: [react()],
   base: '/AIM-Partners/',
   build: {
     outDir: 'docs'
