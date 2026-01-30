47b1746c18dfe74ff6397a980aae2acb92853cb3
brightestleaf <marcodietrich23@gmail.com>
2026-01-30 15:39:27 +0100

    bleh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-cname',
+      closeBundle() {
+        copyFileSync('CNAME', 'dist/CNAME')
+      }
+    }
+  ],
   base: '/AIM-Partners/',
 })
